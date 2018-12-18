2946c0d8a44d0e6ced8641c2ba84c693bcce576e
Nicholas <nenglish7721@gmail.com>
2018-12-17 23:07:17 -0500

    Update PasswordHashingTest.php

--- tests/PasswordHashingTest.php
+++ tests/PasswordHashingTest.php
@@ -15,10 +15,93 @@ class PasswordHashingTest extends TestCase
             'argon_threads' => \PASSWORD_ARGON2_DEFAULT_THREADS,
         ];
         $passwordHasher = new \Viper\PasswordHashing($options);
-        $this->assertTrue(true);
+        $this->assertTrue(\true);
         $options['algo'] = 'argon2i';
         $passwordHasher = new \Viper\PasswordHashing($options);
-        $this->assertTrue(true);
-        // Constructor works!
+        $this->assertTrue(\true);
+    }
+
+    public function testBcryptError1()
+    {
+        $options = [
+            'algo' => 'bcrypt',
+            'bcrypt_cost' => 10,
+            'argon_memory_cost' => \PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+            'argon_time_cost' => \PASSWORD_ARGON2_DEFAULT_TIME_COST,
+            'argon_threads' => \PASSWORD_ARGON2_DEFAULT_THREADS,
+        ];
+        $passwordHasher = new \Viper\PasswordHashing($options);
+        $hash = $passwordHasher->create('55ufbabm5&[-q=`_Br!~K.6NN<rXSsbLQ!A[cd>,"\'4\{5$!JmZupv_@A<R_Pc*m7:9-P{t">T.Q=u&s');
+        $this->expectException(\InvalidArgumentException::class);
+    }
+
+    public function testBcryptError2()
+    {
+        $options = [
+            'algo' => 'bcrypt',
+            'bcrypt_cost' => 10,
+            'argon_memory_cost' => \PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+            'argon_time_cost' => \PASSWORD_ARGON2_DEFAULT_TIME_COST,
+            'argon_threads' => \PASSWORD_ARGON2_DEFAULT_THREADS,
+        ];
+        $passwordHasher = new \Viper\PasswordHashing($options);
+        $hash = $passwordHasher->create('Hello World!');
+        if ($passwordHasher->verify('55ufbabm5&[-q=`_Br!~K.6NN<rXSsbLQ!A[cd>,"\'4\{5$!JmZupv_@A<R_Pc*m7:9-P{t">T.Q=u&s', $hash))
+        {
+        }
+        $this->expectException(\InvalidArgumentException::class);
+    }
+
+    public function testPasswordCreateAndVerify()
+    {
+         $options = [
+            'algo' => 'bcrypt',
+            'bcrypt_cost' => 10,
+            'argon_memory_cost' => \PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+            'argon_time_cost' => \PASSWORD_ARGON2_DEFAULT_TIME_COST,
+            'argon_threads' => \PASSWORD_ARGON2_DEFAULT_THREADS,
+        ];
+        $passwordHasher = new \Viper\PasswordHashing($options);
+        $this->assertTrue(\true);
+        $hash = $passwordHasher->create('Hello World!');
+        $this->assertTrue(\true);
+        if ($passwordHasher->verify('Hello World!', $hash))
+        {
+            $this->assertTrue(\true);
+        } else
+        {
+            $this->assertTrue(\false);
+        }
+        if ($passwordHasher->verify('Hello Tom!', $hash))
+        {
+            $this->assertTrue(\false);
+        } else
+        {
+            $this->assertTrue(\true);
+        }
+        $options = [
+            'algo' => 'bcrypt',
+            'bcrypt_cost' => 15,
+            'argon_memory_cost' => \PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+            'argon_time_cost' => \PASSWORD_ARGON2_DEFAULT_TIME_COST,
+            'argon_threads' => \PASSWORD_ARGON2_DEFAULT_THREADS,
+        ];
+        $passwordHasher2 = new \Viper\PasswordHashing($options);
+        $res = $passwordHasher2->verify('Hello World!', $hash);
+        if (is_array($res))
+        {
+            $this->assertTrue(\true);
+            $verifyNewHash = $res['hash'];
+            if ($passwordHasher2->verify('Hello World!', $verifyNewHash))
+            {
+                $this->assertTrue(\true);
+            } else
+            {
+                $this->assertTrue(\false);
+            }
+        } else
+        {
+            $this->assertTrue(\false);
+        }
     }
 }

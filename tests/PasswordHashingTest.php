226881626afc134bdef261e47e34c9bef3b70b37
Nicholas <45908801+iszorpal@users.noreply.github.com>
2018-12-17 22:18:06 -0500

    Update PasswordHashingTest.php

--- tests/PasswordHashingTest.php
+++ tests/PasswordHashingTest.php
@@ -1,1 +1,14 @@
+<?php
+namespace Viper\PasswordHashing\Test;
 
+use PHPUnit\Framework\TestCase;
+
+class PasswordHashingTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $options = [
+          
+        ];
+    }
+}

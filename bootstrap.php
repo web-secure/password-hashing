7c8d46d4e9b697900befcd087d79c563845315df
Nicholas <45908801+iszorpal@users.noreply.github.com>
2018-12-17 20:24:59 -0500

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+/**
+ * ViperCMS - PasswordHashing.
+ * 
+ * @author Nicholas English <https://github.com/iszorpal>.
+ * @link <https://github.com/iszorpal/password-hashing>.
+ */
+
+defined("PASSWORD_ARGON2I") || define("PASSWORD_ARGON2I", true);
+defined("PASSWORD_ARGON2ID") || define("PASSWORD_ARGON2ID", true);
+defined("PASSWORD_ARGON2_DEFAULT_MEMORY_COST") || define("PASSWORD_ARGON2_DEFAULT_MEMORY_COST", true);
+defined("PASSWORD_ARGON2_DEFAULT_TIME_COST") || define("PASSWORD_ARGON2_DEFAULT_TIME_COST", true);
+defined("PASSWORD_ARGON2_DEFAULT_THREADS") || define("PASSWORD_ARGON2_DEFAULT_THREADS", true);

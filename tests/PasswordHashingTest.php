3457d26a0bd06306108fca21c79ed0cc3393edfe
Nicholas <45908801+iszorpal@users.noreply.github.com>
2018-12-17 22:20:01 -0500

    Update PasswordHashingTest.php

--- tests/PasswordHashingTest.php
+++ tests/PasswordHashingTest.php
@@ -8,7 +8,13 @@ class PasswordHashingTest extends TestCase
     public function testConstructor()
     {
         $options = [
-          
+            'algo' => 'bcrypt',
+            'bcrypt_cost' => 10,
+            'argon_memory_cost' => \PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+            'argon_time_cost' => \PASSWORD_ARGON2_DEFAULT_TIME_COST,
+            'argon_threads' => \PASSWORD_ARGON2_DEFAULT_THREADS,
         ];
+        $passwordHasher = new \Viper\PasswordHashing($options);
+        $this->assertTrue(true);
     }
 }
